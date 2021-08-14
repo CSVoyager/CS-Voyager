a33187250d6103d5b09fbdcdab88c2d0b9a5ebb5
Krish Yadav <44264836+krishyadav007@users.noreply.github.com>
2021-08-14 13:17:29 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -248,7 +248,7 @@ year_element.innerHTML=year_abs;
 
 
 //TIMER CODE
-var countDownDate = new Date("Aug,14 2021 13:17:00").getTime();
+var countDownDate = new Date("Aug,14 2021 13:20:00").getTime();
 var timer_ele = document.getElementById("timer");
 var status_h = document.getElementById("status-head");
 var anchor = document.getElementById("status-anchor");
